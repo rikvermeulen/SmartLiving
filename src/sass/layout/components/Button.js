907e62f07a7a6f2c38b677ba06c4cb5ea3133481
rikvermneulen <rik.vermeulen.1997@live.nl>
2021-05-11 10:55:14 +0200

    fix scss mapping

--- src/sass/layout/components/Button.js
+++
@@ -1,8 +0,0 @@
-import Button from 'react-toolbox/lib/button/Button';
-import buttonTheme from './theme/button.scss';
-
-const ThemedButton = (props) => (
-  <Button theme={buttonTheme}  {...props} />
-);
-
-export default ThemedButton;
